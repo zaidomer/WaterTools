0241a05c95f937224187e210051659de05116ef3
Zaid Omer <zaidomerone@gmail.com>
2020-01-08 02:34:50 -0500

    Test dashboard change

--- app/src/main/java/com/example/watertools/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/watertools/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Welcome to Waterno");
+        mText.setValue("Welcome to Waterzaid");
     }
 
     public LiveData<String> getText() {
