422d8236aca422433216c818eee26372b7a302e7
hanswang0608 <57380995+hanswang0608@users.noreply.github.com>
2020-01-08 23:22:03 -0500

    Removed Back Arrow

--- app/src/main/java/com/example/watertools/MainActivity.java
+++ app/src/main/java/com/example/watertools/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_portal, R.id.navigation_quest, R.id.navigation_crowdmark)
+                R.id.navigation_portal, R.id.navigation_quest, R.id.navigation_crowdmark, R.id.navigation_learn)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
