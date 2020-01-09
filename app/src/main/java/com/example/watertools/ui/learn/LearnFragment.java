fb5a8e4fec81acdd4d31a03a90e556732f19e9e6
Zaid Omer <zaidomerone@gmail.com>
2020-01-08 19:00:24 -0500

    Beginning of webview for learn

--- app/src/main/java/com/example/watertools/ui/learn/LearnFragment.java
+++ app/src/main/java/com/example/watertools/ui/learn/LearnFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -16,18 +18,21 @@ public class LearnFragment extends Fragment {
 
     private LearnViewModel LearnViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        LearnViewModel =
-                ViewModelProviders.of(this).get(LearnViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LearnViewModel = ViewModelProviders.of(this).get(LearnViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_learn, container, false);
+        final TextView textView = root.findViewById(R.id.text_learn);
         LearnViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        WebView learnWebView = (WebView)root.findViewById(R.id.learnWebView);
+        learnWebView.getSettings().setJavaScriptEnabled(true);
+        learnWebView.setWebViewClient(new WebViewClient());
+        learnWebView.loadUrl("https://learn.uwaterloo.ca");
         return root;
     }
 }
