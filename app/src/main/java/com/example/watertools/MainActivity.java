a80d7342fe009250a9a2c2280fb062ef426c357f
hanswang0608 <57380995+hanswang0608@users.noreply.github.com>
2020-01-09 19:03:23 -0500

    Changed appbar and quest icon

--- app/src/main/java/com/example/watertools/MainActivity.java
+++ app/src/main/java/com/example/watertools/MainActivity.java
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_portal, R.id.navigation_quest, R.id.navigation_crowdmark, R.id.navigation_learn, R.id.navigation_works)
-                .build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_portal, R.id.navigation_quest, R.id.navigation_crowdmark, R.id.navigation_learn, R.id.navigation_works)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
